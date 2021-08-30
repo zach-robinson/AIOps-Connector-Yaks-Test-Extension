a46b781dae61ea56047cb85af84fdc9808d3174a
Zach Robinson <ZachRobinson@ibm.com>
2021-08-30 14:06:11 -0500

    testing

---
+++ src/main/java/com/ibm/aiops/connectors/steps/MySteps.java
@@ -0,0 +1,24 @@
+package com.ibm.aiops.connectors.steps;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
+
+public class MySteps {
+    @CitrusResource
+    private TestCaseRunner runner;
+
+    @Given("^My steps are loaded$")
+    public void stepsAreLoaded() {
+        runner.given(echo("My steps are loaded"));
+    }
+
+    @Then("^I can do whatever I want!$")
+    public void doWhateverYouWant() {
+        runner.given(echo("I can do whatever I want!"));
+
+    }
+}
