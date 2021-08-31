db03ef8ed7f480047722a8bf8d67d24ca24e8d04
Zach Robinson <ZachRobinson@ibm.com>
2021-08-31 09:57:10 -0500

    add additional status test steps for ConnectorComponent

--- src/main/java/com/ibm/aiops/connectors/steps/ConnectorComponentSteps.java
+++ src/main/java/com/ibm/aiops/connectors/steps/ConnectorComponentSteps.java
@@ -14,6 +14,7 @@ import io.kubernetes.client.extended.kubectl.exception.KubectlException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.util.Config;
 import io.kubernetes.client.util.ModelMapper;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.citrusframework.yaks.kubernetes.KubernetesSettings;
 
 import java.io.IOException;
@@ -26,7 +27,7 @@ public class ConnectorComponentSteps {
     private Integer stopAfterAttempts = 6;
 
     @Before
-    public void before(Scenario scenario) throws IOException {
+    public void before(@SuppressWarnings("unused") Scenario scenario) throws IOException {
         Configuration.setDefaultApiClient(Config.fromCluster());
         ModelMapper.addModelMap("connectors.aiops.ibm.com", "v1beta1", "ConnectorComponent",
                 "connectorcomponents", true,
@@ -43,7 +44,7 @@ public class ConnectorComponentSteps {
         return Kubectl.get(V1beta1ConnectorComponent.class).name(name).namespace(namespace).execute();
     }
 
-    @Then("^The phase of ConnectorComponent ([^\\s]+) should be ([^\\s]+)")
+    @Then("^The phase of ConnectorComponent ([^\\s]+) should be ([^\\s]+)$")
     public void testPhase(String name, String expectedPhase) {
         Polling.waitPeriodly(waitSeconds, TimeUnit.SECONDS).stopAfterAttempt(stopAfterAttempts).run(() -> {
             try {
@@ -58,5 +59,58 @@ public class ConnectorComponentSteps {
                 return AttemptResults.justContinue();
             }
         });
+    }
+
+    @Then("^The requeueAfter of ConnectorComponent ([^\\s]+) should be (\\d+)$")
+    public void testRqAfter(String name, Long expectedRqAfter) {
+        Polling.waitPeriodly(waitSeconds, TimeUnit.SECONDS).stopAfterAttempt(stopAfterAttempts).run(() -> {
+            try {
+                final V1beta1ConnectorComponent connectorComponent = getConnectorComponent(name);
+                final Long actualRqAfter = Objects.requireNonNull(connectorComponent.getStatus()).getRequeueAfter();
+                if (!Objects.equals(actualRqAfter, expectedRqAfter)) {
+                    throw new CitrusRuntimeException("Expected requeueAfter of ConnectorComponent "
+                            + name + " to be " + expectedRqAfter + ", but was " + actualRqAfter);
+                }
+                return AttemptResults.justFinish();
+            } catch (Exception e) {
+                return AttemptResults.justContinue();
+            }
+        });
+    }
+
+    public void testMessageHelper(String name, String expectedKey, String expectedData) {
+        Polling.waitPeriodly(waitSeconds, TimeUnit.SECONDS).stopAfterAttempt(stopAfterAttempts).run(() -> {
+            try {
+                final V1beta1ConnectorComponent connectorComponent = getConnectorComponent(name);
+                if (!Objects.requireNonNull(Objects.requireNonNull(connectorComponent.getStatus()).getMessages())
+                        .containsKey(expectedKey)) {
+                    throw new CitrusRuntimeException("status.messages of ConnectorComponent " + name +
+                            " did not contain key: " + expectedKey);
+                }
+                if (expectedData != null) {
+                    final String actualData = Objects.requireNonNull(connectorComponent.getStatus().getMessages())
+                            .get(expectedKey).toString();
+                    if (!actualData.equals(expectedData)) {
+                        throw new CitrusRuntimeException("Expected status.messages." + expectedKey +
+                                " of ConnectorComponent " + name + " to have value of '" +
+                                StringEscapeUtils.escapeJava(expectedData) + "', but was '" +
+                                StringEscapeUtils.escapeJava(actualData) + "'");
+                    }
+                }
+                return AttemptResults.justFinish();
+            } catch (Exception e) {
+                return AttemptResults.justContinue();
+            }
+        });
+    }
+
+    @Then("^The status.messages of ConnectorComponent ([^\\s]+) should contain the key ([^\\s]+)$")
+    public void testMessageKey(String name, String expectedKey) {
+        testMessageHelper(name, expectedKey, null);
+    }
+
+    @Then("^The status.messages of ConnectorComponent ([^\\s]+) should contain the key ([^\\s]+) which should have data matching ([^\\s]+)$")
+    public void testMessageBody(String name, String expectedKey, String expectedData) {
+        testMessageHelper(name, expectedKey, expectedData);
     }
 }
